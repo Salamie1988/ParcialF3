c252344396e4dfbb3d80e3391fd9a205df5e5408
salamie1988 <salamierodrigo@gmail.com>
2024-05-23 20:47:32 -0300

    Terminado

--- src/Components/Mascota.jsx
+++ src/Components/Mascota.jsx
@@ -1,19 +1,18 @@
 
 
 const NombreMascota = ({ value, onChange }) => {
-  return (
-    <div>
-      <p id="nombreMascotaDesc">Introduce el nombre de tu mascota:</p>
-      <input
-        type="text"
-        id="nombreMascota"
-        name="nombreMascota"
-        aria-describedby="nombreMascotaDesc"
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <p id="nombreMascotaDesc"></p>
+            <input
+                type="text"
+                id="nombreMascota"
+                name="nombreMascota"
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
 };
 
 export default NombreMascota;
--- src/Components/Raza.jsx
+++ src/Components/Raza.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 
 const RazaMascota = ({ value, onChange }) => {
-  return (
-    <div>
-      <p id="razaMascotaDesc">Introduce la raza de tu mascota:</p>
-      <input
-        type="text"
-        id="razaMascota"
-        name="razaMascota"
-        aria-describedby="razaMascotaDesc"
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <p id="razaMascotaDesc"></p>
+            <input
+                type="text"
+                id="razaMascota"
+                name="razaMascota"
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
 };
 
 export default RazaMascota;
